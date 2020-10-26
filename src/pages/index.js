7076b15d743fc0624761198658282e9579e2c4c5
Marichelle Bisuna <marichellebisuna@yahoo.com>
2020-10-26 18:17:18 +1100

    checked error

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-//import "bootstrap/dist/js/bootstrap"
 import { GatsbyProvider } from "../context/context"
-
-import $ from "jquery"
-import "../components/layout.css"
-import "../css/classy-nav.min.css"
-
-var searchwrapper = $(".search-wrapper")
-$(".search-btn").on("click", function () {
-  searchwrapper.toggleClass("on")
-})
-$(".close-btn").on("click", function () {
-  searchwrapper.removeClass("on")
-})
-// :: 3.0 Nav Active Code
-if ($.fn.classyNav) {
-  $("#deliciousNav").classyNav()
-}
 
 const IndexPage = () => {
   return (
